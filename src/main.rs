b26bff09046a122bd8cd79bdf50b5806c54a9ba0
rudolfmuller <rudolfmullercz@gmail.com>
2026-04-18 15:40:43 +0200

    added placeholders

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,60 @@
-use chrono::Local;
+use strfmt::strfmt;
+mod sysstat;
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
-use sysinfo::{Disks, System};
-fn main() {
-    let mut system = System::new_all();
-    let mut disks = Disks::new();
+
+use chrono::{Datelike, Timelike};
+
+fn main() -> anyhow::Result<()> {
+    let mut stat = sysstat::Status::new();
+    let mut vars = HashMap::new();
     loop {
-        let now = Local::now();
-        system.refresh_all();
-        disks.refresh(false);
-        let free_disk = if let Some(disk) = disks.list().first() {
-            let total = disk.total_space();
-            let available = disk.available_space();
-
-            (available as f32 / total as f32) * 100.0
-        } else {
-            0.0
-        };
-        let used_cpu = system.global_cpu_usage();
-        let used_memory = (system.used_memory() as f32 / system.total_memory() as f32) * 100.0;
-
-        println!(
-            " {:>6.2}󱉸 󰋊 {:>3.0}󱉸  {:>6.2}󱉸 [{}]",
-            used_memory,
-            free_disk,
-            used_cpu,
-            now.format("%d-%m.%y %a %H:%M:%S"),
+        stat.refresh();
+        vars.insert(
+            "disk.free".to_string(),
+            format!("{:>3.0}", stat.free_disk()),
         );
+        vars.insert(
+            "cpu.used".to_string(),
+            format!("{:>6.2}", stat.global_used_cpu()),
+        );
+        vars.insert(
+            "memory.used".to_string(),
+            format!("{:>6.2}", stat.used_memory()),
+        );
+
+        vars.insert("date.year".to_string(), stat.now.year().to_string());
+        vars.insert(
+            "date.year.short".to_string(),
+            format!("{:02}", stat.now.year() % 100),
+        );
+        vars.insert("date.month".to_string(), format!("{:02}", stat.now.month()));
+        vars.insert("date.day".to_string(), format!("{:02}", stat.now.day()));
+
+        vars.insert("time.hour".to_string(), format!("{:02}", stat.now.hour()));
+        vars.insert("time.min".to_string(), format!("{:02}", stat.now.minute()));
+        vars.insert("time.sec".to_string(), format!("{:02}", stat.now.second()));
+
+        vars.insert(
+            "date.weekday".to_string(),
+            format!("{:?}", stat.now.weekday()),
+        );
+        vars.insert(
+            "date.weekday.short".to_string(),
+            format!("{:?}", stat.now.weekday())
+                .chars()
+                .take(3)
+                .collect(),
+        );
+
+        let fmt = " {memory.used}󱉸 󰋊 {disk.free}󱉸  {cpu.used}󱉸 [{date.day}-{date.month}.{date.year.short} {date.weekday.short} {time.hour}:{time.min}:{time.sec}]".to_string();
+        println!("{}", strfmt(&fmt, &vars)?);
+
+        /*println!(
+            " {used_memory:>6.2}󱉸 󰋊 {free_disk:>3.0}󱉸  {used_cpu:>6.2}󱉸 [{dd:02}-{mm:02}.{y:02} {a:03} {hh:02}:{min:02}:{ss:02}]",
+        );*/
+
         thread::sleep(Duration::from_millis(1000));
     }
 }
