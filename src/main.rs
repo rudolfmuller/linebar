184bd94932f9be1015eac30688758c2d36896c51
rudolfmuller <rudolfmullercz@gmail.com>
2026-04-19 10:44:33 +0200

    added new function 'build_format_scope()'

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ use std::thread;
 use std::time::Duration;
 use strfmt::strfmt;
 use thiserror::Error;
+
+use crate::sysstat::Status;
 mod sysstat;
 
 const SWAY_CONFIG_DIR_NAME: &str = "sway";
 const LINEBAR_TOML_FILE_NAME: &str = "linebar.toml";
 
+// TOML Config
 #[derive(Deserialize)]
 struct Config {
     general: General,
@@ -37,7 +40,7 @@ pub enum LineBarError {
     #[error("format error")]
     FormatError(#[from] strfmt::FmtError),
 }
-
+// Return sway configuration directory
 fn sway_config_dir() -> Result<PathBuf, LineBarError> {
     let sway_config_dir = dirs::config_dir()
         .ok_or(LineBarError::DirectoryError)?
@@ -45,54 +48,58 @@ fn sway_config_dir() -> Result<PathBuf, LineBarError> {
     Ok(sway_config_dir)
 }
 
+fn build_format_scope(stat: &Status) -> HashMap<String, String> {
+    let mut vars = HashMap::new();
+    vars.insert(
+        "disk.free".to_string(),
+        format!("{:>3.0}", stat.free_disk()),
+    );
+    vars.insert(
+        "disk.used".to_string(),
+        format!("{:>3.0}", stat.used_disk()),
+    );
+
+    vars.insert(
+        "cpu.used".to_string(),
+        format!("{:>6.2}", stat.global_used_cpu()),
+    );
+    vars.insert(
+        "memory.used".to_string(),
+        format!("{:>6.2}", stat.used_memory()),
+    );
+    vars.insert(
+        "memory.free".to_string(),
+        format!("{:>6.2}", stat.free_memory()),
+    );
+
+    vars.insert("date.year".to_string(), stat.now.year().to_string());
+    vars.insert(
+        "date.year.short".to_string(),
+        format!("{:02}", stat.now.year() % 100),
+    );
+    vars.insert("date.month".to_string(), format!("{:02}", stat.now.month()));
+    vars.insert("date.day".to_string(), format!("{:02}", stat.now.day()));
+
+    vars.insert("time.hour".to_string(), format!("{:02}", stat.now.hour()));
+    vars.insert("time.min".to_string(), format!("{:02}", stat.now.minute()));
+    vars.insert("time.sec".to_string(), format!("{:02}", stat.now.second()));
+
+    vars.insert(
+        "date.weekday".to_string(),
+        format!("{:?}", stat.now.weekday()),
+    );
+    vars
+}
+
 fn main() -> Result<(), LineBarError> {
     let mut stat = sysstat::Status::new();
     let cfg_path = sway_config_dir()?.join(LINEBAR_TOML_FILE_NAME);
     let config: Config = toml::from_str(&fs::read_to_string(cfg_path)?)?;
     loop {
-        let mut vars = HashMap::new();
         stat.refresh();
-        vars.insert(
-            "disk.free".to_string(),
-            format!("{:>3.0}", stat.free_disk()),
-        );
-        vars.insert(
-            "disk.used".to_string(),
-            format!("{:>3.0}", stat.used_disk()),
-        );
-
-        vars.insert(
-            "cpu.used".to_string(),
-            format!("{:>6.2}", stat.global_used_cpu()),
-        );
-        vars.insert(
-            "memory.used".to_string(),
-            format!("{:>6.2}", stat.used_memory()),
-        );
-        vars.insert(
-            "memory.free".to_string(),
-            format!("{:>6.2}", stat.free_memory()),
-        );
-
-        vars.insert("date.year".to_string(), stat.now.year().to_string());
-        vars.insert(
-            "date.year.short".to_string(),
-            format!("{:02}", stat.now.year() % 100),
-        );
-        vars.insert("date.month".to_string(), format!("{:02}", stat.now.month()));
-        vars.insert("date.day".to_string(), format!("{:02}", stat.now.day()));
-
-        vars.insert("time.hour".to_string(), format!("{:02}", stat.now.hour()));
-        vars.insert("time.min".to_string(), format!("{:02}", stat.now.minute()));
-        vars.insert("time.sec".to_string(), format!("{:02}", stat.now.second()));
-
-        vars.insert(
-            "date.weekday".to_string(),
-            format!("{:?}", stat.now.weekday()),
-        );
-
-        //let fmt = " {memory.used}󱉸 󰋊 {disk.free}󱉸  {cpu.used}󱉸 [{date.day}-{date.month}.{date.year.short} {date.weekday} {time.hour}:{time.min}:{time.sec}]".to_string();
-        println!("{}", strfmt(&config.general.format, &vars)?);
+        let fmt_scope = build_format_scope(&stat);
+        let formated = strfmt(&config.general.format, &fmt_scope)?;
+        println!("{formated}");
         io::stdout().flush()?;
 
         thread::sleep(Duration::from_millis(config.general.interval));
