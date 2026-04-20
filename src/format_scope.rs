f05032b3aa5ac26f40033a2accaae6718df97117
rudolfmuller <rudolfmullercz@gmail.com>
2026-04-20 14:43:18 +0200

    rename 'format_scope::insert()' -> 'format_scope::insert_var()'

--- src/format_scope.rs
+++ src/format_scope.rs
@@ -5,39 +5,39 @@ use std::collections::HashMap;
 pub fn build_format_scope(stat: &Status) -> HashMap<String, String> {
     let mut vars = HashMap::new();
 
-    insert(&mut vars, "disk.free", format!("{:>3.0}", stat.free_disk()));
-    insert(&mut vars, "disk.used", format!("{:>3.0}", stat.used_disk()));
+    insert_var(&mut vars, "disk.free", format!("{:>3.0}", stat.free_disk()));
+    insert_var(&mut vars, "disk.used", format!("{:>3.0}", stat.used_disk()));
 
-    insert(
+    insert_var(
         &mut vars,
         "cpu.used",
         format!("{:>6.2}", stat.global_used_cpu()),
     );
-    insert(
+    insert_var(
         &mut vars,
         "memory.used",
         format!("{:>6.2}", stat.used_memory()),
     );
-    insert(
+    insert_var(
         &mut vars,
         "memory.free",
         format!("{:>6.2}", stat.free_memory()),
     );
 
-    insert(&mut vars, "date.year", stat.now.year());
-    insert(
+    insert_var(&mut vars, "date.year", stat.now.year().to_string());
+    insert_var(
         &mut vars,
         "date.year.short",
         format!("{:02}", stat.now.year() % 100),
     );
-    insert(&mut vars, "date.month", format!("{:02}", stat.now.month()));
-    insert(&mut vars, "date.day", format!("{:02}", stat.now.day()));
+    insert_var(&mut vars, "date.month", format!("{:02}", stat.now.month()));
+    insert_var(&mut vars, "date.day", format!("{:02}", stat.now.day()));
 
-    insert(&mut vars, "time.hour", format!("{:02}", stat.now.hour()));
-    insert(&mut vars, "time.min", format!("{:02}", stat.now.minute()));
-    insert(&mut vars, "time.sec", format!("{:02}", stat.now.second()));
+    insert_var(&mut vars, "time.hour", format!("{:02}", stat.now.hour()));
+    insert_var(&mut vars, "time.min", format!("{:02}", stat.now.minute()));
+    insert_var(&mut vars, "time.sec", format!("{:02}", stat.now.second()));
 
-    insert(
+    insert_var(
         &mut vars,
         "date.weekday",
         format!("{:?}", stat.now.weekday()),
@@ -46,6 +46,6 @@ pub fn build_format_scope(stat: &Status) -> HashMap<String, String> {
     vars
 }
 
-fn insert<T: ToString>(vars: &mut HashMap<String, String>, key: &str, value: T) {
-    vars.insert(key.to_string(), value.to_string());
+fn insert_var(vars: &mut HashMap<String, String>, key: &str, value: String) {
+    vars.insert(key.to_string(), value);
 }
