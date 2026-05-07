9bef6b8b3c5a24fac01d6843865b1323a6604bbd
rudolfmuller <rudolfmullercz@gmail.com>
2026-05-07 22:22:32 +0200

    new version 1.0.1: remove field 'disks' from 'Status', the disks are created directly in the 'free_disk()' and 'used_disk()' functions
    => big optimization

--- src/stat.rs
+++ src/stat.rs
@@ -4,46 +4,47 @@ use sysinfo::{Disks, System};
 #[derive(Debug)]
 pub struct Status {
     system: System,
-    disks: Disks,
     pub now: DateTime<Local>,
     pub remove_not_listed_disks: bool,
 }
 impl Status {
     pub fn new() -> Self {
         let system = System::new_all();
-        let disks = Disks::new();
         let now = Local::now();
         Self {
             system,
-            disks,
             now,
             remove_not_listed_disks: false,
         }
     }
     pub fn refresh(&mut self) {
-        self.system.refresh_all();
-        self.disks.refresh(self.remove_not_listed_disks);
+        self.system.refresh_memory();
+        self.system.refresh_cpu_all();
         self.now = Local::now();
     }
     pub fn free_disk(&self) -> f32 {
-        if let Some(disk) = self.disks.list().first() {
-            let total = disk.total_space();
-            let available = disk.available_space();
+        let disks = Disks::new_with_refreshed_list();
 
-            return (available as f32 / total as f32) * 100.0;
+        if let Some(disk) = disks.list().first() {
+            let total = disk.total_space() as f32;
+            let available = disk.available_space() as f32;
+
+            (available / total) * 100.0
         } else {
-            return 0.0;
-        };
+            0.0
+        }
     }
     pub fn used_disk(&self) -> f32 {
-        if let Some(disk) = self.disks.list().first() {
-            let total = disk.total_space();
-            let available = disk.available_space();
+        let disks = Disks::new_with_refreshed_list();
 
-            return (total as f32 - available as f32) / total as f32 * 100.0;
+        if let Some(disk) = disks.list().first() {
+            let total = disk.total_space() as f32;
+            let available = disk.available_space() as f32;
+
+            ((total - available) / total) * 100.0
         } else {
-            return 0.0;
-        };
+            0.0
+        }
     }
     pub fn global_used_cpu(&self) -> f32 {
         self.system.global_cpu_usage()
